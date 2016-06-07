dd494367df6b9f157b56406cbb173b7e91b75783
emir-acloud <emir.uzeirbegovic@theaccessgroup.com>
2016-06-07 11:59:09 +0100

    Initial.

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # textextract
-Find and extract article text from HTML for Go
+
+textextract is a tiny library (87 lines of Go) that identifies where the article content is in a HTML page (as opposed to navigation, headers, footers, ads, etc), extracts it and returns it as a string.
+
+It's a tree search and score algorithm, it uses a very simple scoring rule, it is surprisingly effective.
+
+## What it's for
+
+If you're doing semantic analysis on crawled information and you need article content to feed into some other process like a semantic extractor, classifier, etc. Note however that whilst it preserves the rendering order of text it doesn't preserve white space.
+
+## How it works
+
+1. It parses the HTML into a node tree using the standard Go html package.
+
+2. It then walks the tree depth first and scores each node on route. The score from the parent node is pushed down as the basis for the child node. The scoring formula is essentially: WORDCOUNT - WORDCOUNTINANCHOR^2, where WORDCOUNT is the number of words in the node that are not hyperlinked and WORDCOUNTINANCHOR are the number of words in the node that are hyperlinked. The WORDCOUNTINANCHOR for each node is actually calculated as 1 + WORDCOUNTINACHOR^2 just because there is often anchors on things other than words so WORDCOUNTINACHOR^2 is often zero.
+
+3. As it goes it'll add nodes that are below the minimum score to a toDelete slice. When the recursion is finished, it'll delete all nodes in the toDelete slice.
+
+4. Finally, the filtered tree is parsed again, depth first, and all text nodes are printed to a string.
+
+## How to install it
+
+    go get https://github.com/emiruz/textextract
+
+## How to use it
+
+    import "github.com/emiruz/textextract"
+
+    main func() {
+        extractedText, err := textextract.Extract(yourUT8HTMLString)
+    }
+
+## License
+
+MIT Licensed, do as you will with it.
+
+## Bugs
+
+Please submit them as issues on the repository.
+
+## TODO
+
+1. Add tests
+
+2. Add comments.
---
+++ textextract.go
@@ -0,0 +1,96 @@
+package textextract
+
+import ("strings"
+	"golang.org/x/net/html"
+	"bytes"
+	"fmt"
+	"regexp"
+	"errors"
+)
+
+var MinScore = 5
+
+func isInAnchor(n *html.Node) bool {
+	if n.Parent == nil {
+		return false
+	}
+	if n.Parent.Data == "a" {
+		return true
+	}
+	return isInAnchor(n.Parent)
+}
+
+func normaliseText(t string) string {
+	r, _ := regexp.Compile("<[^>]*>|\\n|\\t| +")	
+	r2, _ := regexp.Compile("^ +| +$")
+	return r2.ReplaceAllString(r.ReplaceAllString(
+		r.ReplaceAllString(t, " "),
+		" "),"")
+}
+
+func filter(doc *html.Node, minScore int) *html.Node {
+	type NodePair struct {
+		Parent *html.Node
+		Child *html.Node
+	}
+	toDelete := []NodePair{}
+	var f func(n *html.Node, score int) int
+	f = func(n *html.Node, score int) int {
+		if n.Type == html.TextNode {
+			count := len(strings.Split(normaliseText(n.Data), " "))
+			switch {
+			case n.Parent.Data == "script":
+			case n.Parent.Data == "style":
+			case n.Parent.Data == "link":
+			case isInAnchor(n):
+				score -= 1 + count^2
+			default:
+				score += count
+			}
+			return score
+		}
+
+		ownScore := score
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			score += f(c, ownScore)
+		}
+
+		if score <= minScore && n.Data != "a"  {
+			toDelete = append(toDelete, NodePair{n.Parent,n})
+		}
+		return score
+	}
+	f(doc,0)
+
+	for _, x := range toDelete {
+		if x.Parent != nil {
+			x.Parent.RemoveChild(x.Child)
+		}
+	}
+	return doc
+}
+
+func ExtractFromText(htmlUTF8Str string) (string,error) {
+	doc, err := html.Parse(strings.NewReader(htmlUTF8Str))
+	if err != nil {
+		return "", errors.New("Could not parse HTML string.")
+	}
+	doc = filter(doc, MinScore)
+	var f func(n *html.Node)
+	var buffer bytes.Buffer
+	f = func(n *html.Node) {
+		d := normaliseText(n.Data)
+		if n.Type == html.TextNode && d != "" && d!= " " {
+			switch n.Parent.Data {
+			case "title":
+			default:
+				buffer.WriteString(fmt.Sprintf("\n%s", d))
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	f(doc)
+	return buffer.String(), nil
+}
